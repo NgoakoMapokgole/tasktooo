8e46da1cf6f3aa43c17edef952f470074fe5f188
Ngoako Mapokgole <100357050+NgoakoMapokgole@users.noreply.github.com>
2024-05-22 21:52:33 +0000

    Step 3: Output user-selected fields in JSON format

--- XMLReader.java
+++ XMLReader.java
@@ -6,8 +6,43 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import java.io.File;
 import java.util.Scanner;
+import org.json.simple.JSONObject;
 
-// Add these lines to the main method after reading the XML
+
+
+
+public class XMLReader {
+    public static void main(String[] args) {
+        try {
+            File inputFile = new File("/workspaces/tasktooo/xmlfile.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+
+            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
+
+            NodeList nList = doc.getElementsByTagName("name");
+            for (int i = 0; i < nList.getLength(); i++) {
+                Node nNode = nList.item(i);
+                System.out.println("\nCurrent Element: " + nNode.getNodeName());
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    System.out.println("Field1: " + eElement.getElementsByTagName("name").item(0).getTextContent());
+                    System.out.println("Field2: " + eElement.getElementsByTagName("postalZip").item(0).getTextContent());
+                    System.out.println("Field1: " + eElement.getElementsByTagName("Region").item(0).getTextContent());
+                    System.out.println("Field2: " + eElement.getElementsByTagName("country").item(0).getTextContent());
+                    System.out.println("Field1: " + eElement.getElementsByTagName("address").item(0).getTextContent());
+                    System.out.println("Field2: " + eElement.getElementsByTagName("list").item(0).getTextContent());
+                    // Add more fields as necessary
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    
+    
+
 Scanner scanner = new Scanner(System.in);
 System.out.println("Enter the fields you want to print (comma separated):");
 String input = scanner.nextLine();
@@ -29,31 +64,25 @@ for (int i = 0; i < nList.getLength(); i++) {
     }
 }
 scanner.close();
+JSONObject jsonObject = new JSONObject();
 
-public class XMLReader {
-    public static void main(String[] args) {
-        try {
-            File inputFile = new File("path/to/your/file.xml");
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(inputFile);
-            doc.getDocumentElement().normalize();
-
-            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
-
-            NodeList nList = doc.getElementsByTagName("yourElementTagName");
-            for (int i = 0; i < nList.getLength(); i++) {
-                Node nNode = nList.item(i);
-                System.out.println("\nCurrent Element: " + nNode.getNodeName());
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) nNode;
-                    System.out.println("Field1: " + eElement.getElementsByTagName("field1").item(0).getTextContent());
-                    System.out.println("Field2: " + eElement.getElementsByTagName("field2").item(0).getTextContent());
-                    // Add more fields as necessary
-                }
+for (int i = 0; i < nList.getLength(); i++) {
+    Node nNode = nList.item(i);
+    if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+        Element eElement = (Element) nNode;
+        JSONObject fieldObject = new JSONObject();
+        for (String field : fields) {
+            field = field.trim();
+            try {
+                fieldObject.put(field, eElement.getElementsByTagName(field).item(0).getTextContent());
+            } catch (Exception e) {
+                fieldObject.put(field, "Field not found");
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+        jsonObject.put("Element " + i, fieldObject);
+    }
+}
+System.out.println(jsonObject.toJSONString());
+
     }
 }
